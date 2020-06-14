77cc837833128b62f405eec13158133675d87291
TakeshiHora <a1936th@aiit.ac.jp>
2020-06-14 23:27:56 +0900

    made Factory Pattern

---
+++ src/Adaptor/Print.java
@@ -0,0 +1,6 @@
+package Adaptor;
+
+public interface Print {
+    public abstract void printWeek();
+    public abstract void printStrong();
+}
---
+++ src/Adaptor/PrintBanner.java
@@ -0,0 +1,13 @@
+package Adaptor;
+
+public class PrintBanner extends Banner implements Print {
+    public PrintBanner(String string) {
+        super(string);
+    }
+    public void printWeek() {
+        showWithParen();
+    }
+    public void printStrong() {
+        showWithAster();
+    }
+}
